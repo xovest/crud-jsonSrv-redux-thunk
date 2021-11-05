e947b8ebb532401419329df8649c9157f850c98c
xovest <79008917+xovest@users.noreply.github.com>
2021-11-05 17:30:46 +0200

    added the table with struggles

---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function Home() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650, marginTop: 100 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="center">Email</TableCell>
+            <TableCell align="center">Contact</TableCell>
+            <TableCell align="center">Adress</TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="center">{row.calories}</TableCell>
+              <TableCell align="center">{row.fat}</TableCell>
+              <TableCell align="center">{row.carbs}</TableCell>
+              <TableCell align="center">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/redux/actions.js
@@ -0,0 +1,10 @@
+import * as types from './actionType';
+
+const getUsers = (users) => ({
+  type: type.GET_USERS,
+  payload: users
+});
+
+export const loadUsers = () => {
+
+};
